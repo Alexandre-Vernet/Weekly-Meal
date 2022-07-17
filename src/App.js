5b79b066949401724f1d454826e4f15beb2f6932
Alex <alexandre.vernet@g-mail.fr>
2022-07-17 19:59:06 +0200

    Feat - Install R-Suite

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
-import './App.css';
 import { Cooking } from "./Cooking";
+import { Button } from 'rsuite';
 
 export class App extends React.Component {
+    state = {
+        cooking: Cooking.getCookingForWeeks()
+    }
+    getCooking() {
+        this.setState({
+            cooking: Cooking.getCookingForWeeks()
+        });
+    }
+
     render() {
         return (
             <div>
-                <h1>Tu vas manger : </h1>
-                <span>{ Cooking.getCookingForWeeks() }</span>
+                <h1>Repas du jour :</h1>
+                <span>{ this.state.cooking }</span>
+                <Button appearance="primary" onClick={ () => this.getCooking() }>Nouvelle recette</Button>
             </div>
         );
     }
