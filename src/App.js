1a62069598895639c73158680de73105d27e2869
Alex <alexandre.vernet@g-mail.fr>
2022-07-18 20:14:48 +0200

    Feat - Display R-Suite table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React from "react";
 import { Cooking } from "./Cooking";
 import { Button, Drawer } from 'rsuite';
+import { Table } from 'rsuite';
 
 export class App extends React.Component {
     state = {
-        cooking: Cooking.getCookingForWeeks(),
-        drawerOpen: false
+        cooking: Cooking.getCookingForOneMeal(),
+        weeklyCooking: Cooking.getCookingForWeek(),
+        drawerOpen: false,
+    };
+
+    componentDidMount() {
+        this.getWeekMenu();
     }
 
     getCooking() {
         this.setState({
-            cooking: Cooking.getCookingForWeeks()
+            cooking: Cooking.getCookingForOneMeal()
         });
+    }
+
+    getWeekMenu() {
+        this.setState({
+            weeklyCooking: Cooking.getCookingForWeek()
+        })
     }
 
     render() {
         return (
             <div>
                 <h1>Repas du jour :</h1>
-                <span onClick={ () => this.setState({ drawerOpen: true }) }>{ this.state.cooking.title }</span>
+                <span>{ this.state.cooking.title }</span>
+                <br/>
                 <Button appearance="primary" onClick={ () => this.getCooking() }>Nouvelle recette</Button>
+
+                <Table
+                    data={ this.state.weeklyCooking }
+                    bordered
+                    cellBordered
+                    autoHeight
+                    affixHeader
+                    affixHorizontalScrollbar
+                    onClick={ () => this.setState({ drawerOpen: true }) }
+                >
+
+                    <Table.Column width={ 600 } fixed resizable>
+                        <Table.HeaderCell>Jour</Table.HeaderCell>
+                        <Table.Cell dataKey="description"/>
+                    </Table.Column>
+                    <Table.Column width={ 500 } fixed resizable>
+                        <Table.HeaderCell>Plat</Table.HeaderCell>
+                        <Table.Cell dataKey="title"/>
+                    </Table.Column>
+                </Table>
+
 
                 <Drawer open={ this.state.drawerOpen } onClose={ () => this.setState({ drawerOpen: false }) }>
                     <Drawer.Header>
--- src/Cooking.js
+++ src/Cooking.js
@@ -1,14 +1,19 @@
 import cooking from "./cooking.json";
 
 export class Cooking {
-    static getCookingForWeeks() {
+    static getCookingForOneMeal() {
         // Get cooking length
-        const cookingLength = Object.keys(cooking).length;
+        const cookingLength = cooking.length;
 
         // Get random number
         const randomNumber = Math.floor(Math.random() * cookingLength);
 
         // Get cooking with random number
         return cooking[randomNumber];
+    }
+
+    static getCookingForWeek() {
+        // Get all cooking
+        return cooking;
     }
 }
