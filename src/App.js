dd4c55f13af9d828093f297e43c0fc62404548b1
Alex <alexandre.vernet@g-mail.fr>
2022-07-29 16:17:19 +0200

    Clean - Change BrowserRouter for HashRouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Meals } from "./Meals";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Admin } from "./Admin";
 import { CustomNavbar } from "./CustomNavbar";
 
 export class App extends React.Component {
     render() {
         return (
-            <BrowserRouter>
+            <HashRouter>
                 <CustomNavbar/>
                 <Routes>
                     <Route path="/" element={ <Meals/> }/>
                     <Route path="/admin" element={ <Admin/> }/>
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         )
     }
 }
